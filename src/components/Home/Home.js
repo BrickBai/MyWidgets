22ac4a15fd118c2bc0d4d046f75a9e0d0a1cbb8c
“BrickBai” <bricknight233@gmail.com>
2017-09-26 17:27:19 -0400

    set react router

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Home.css';
+import IdeasContainer from '../IdeasContainer'
+import Clock from '../Clock';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Home-header">
+          <h2>My Widgets in construction \(^o^)/</h2>
+        </div>
+        <div className="Home-row">
+        <Clock num={1}/>
+        </div>
+        <div className="Home-row">
+          <IdeasContainer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
